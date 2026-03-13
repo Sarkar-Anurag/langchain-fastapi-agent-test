3775692947d64d9d0833f6ce254fdc43adc2ed19
Sarkar-Anurag <sarkaranurag007@gmail.com>
2026-03-13 19:31:45 +0530

    test commit

--- tools.py
+++ tools.py
@@ -10,15 +10,16 @@ def multiply(a: int, b: int) -> int:
     """Multiply two numbers"""
     return a * b
 
-@tool
-def divide(a: int, b: int) -> int:
-    """Divide two numbers"""
-    return a / b
+# @tool
+# def divide(a: int, b: int) -> int:
+#     """Divide two numbers"""
+#     return a / b
 
-@tool
-def subtract(a: int, b: int) -> int:
-    """Subtract two numbers"""
-    return a - b
+# @tool
+# def subtract(a: int, b: int) -> int:
+#     """Subtract two numbers"""
+#     return a - b
 
 
-tools = [add, multiply, divide, subtract]
+tools = [add, multiply]
+# tools = [add, multiply, divide, subtract]
